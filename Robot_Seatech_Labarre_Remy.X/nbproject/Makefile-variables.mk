92dd02d6b3f7b851293cf34eaf328c31af9cb15d
Table 1 <touloniut@gmail.com>
2024-10-02 09:39:30 +0200

    Robot1

---
+++ Robot_Seatech_Labarre_Remy.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Robot_Seatech_Labarre_Remy.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Robot_Seatech_Labarre_Remy.X.production.hex
